3f2add185fc56189c574734088b0c832b0f2df0a
Niklas Förstberg <github@forstberg.com>
2025-03-04 17:56:39 +0100

    Add ChallengeCompletion DbSet to ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ public class ApplicationDbContext : DbContext
     public DbSet<Challenge> Challenges { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<TeamUser> TeamUsers { get; set; }
+    public DbSet<ChallengeCompletion> ChallengeCompletions { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
