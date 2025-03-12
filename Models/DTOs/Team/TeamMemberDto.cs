a853b688747f4bce4d0b8a93a5978d655829b736
niklasforstberg <github@forstberg.com>
2025-03-12 20:11:14 +0100

    Remove Role property from TeamMemberDto to streamline team member data structure

--- Models/DTOs/Team/TeamMemberDto.cs
+++ Models/DTOs/Team/TeamMemberDto.cs
@@ -4,6 +4,5 @@ public class TeamMemberDto
 {
     public int UserId { get; set; }
     public string Email { get; set; } = string.Empty;
-    public string Role { get; set; } = string.Empty;
     public DateTime JoinedAt { get; set; }
-} 
+}
