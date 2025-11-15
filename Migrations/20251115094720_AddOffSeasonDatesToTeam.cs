348ebef867ebf46a10c6e8b674d1198ec25dc8e2
Niklas Förstberg <github@forstberg.com>
2025-11-15 10:51:47 +0100

    Added offseasonstart and enddates

---
+++ Migrations/20251115094720_AddOffSeasonDatesToTeam.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MissionComplete.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOffSeasonDatesToTeam : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OffSeasonEndDate",
+                table: "Teams",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OffSeasonStartDate",
+                table: "Teams",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OffSeasonEndDate",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "OffSeasonStartDate",
+                table: "Teams");
+        }
+    }
+}
